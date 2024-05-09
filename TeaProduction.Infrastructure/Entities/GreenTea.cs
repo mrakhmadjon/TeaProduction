84ef8b20eca45b9d16272e32e1eaf23b8782c59e
Akhmadjon <mrjohnaccaount@gmail.com>
2024-05-09 21:39:20 +0200

    Entities were Added

---
+++ TeaProduction.Infrastructure/Entities/GreenTea.cs
@@ -0,0 +1,18 @@
+﻿namespace TeaProduction.Infrastructure.Entities
+{
+    internal class GreenTea : Tea
+    {
+        public double AntioxidantLevel { get; set; }
+        public string LeafGrade { get; set; }
+        public int BrewingTemperature { get; set; }
+        public override string GetDescription()
+        {
+            return $"Name: {Name}," +
+                $" Type: {Type}," +
+                $" Price: {Price}, " +
+                $"Antioxidant level : {AntioxidantLevel}," +
+                $"Leaf grade: {LeafGrade}," +
+                $"Brewing Temperature : {BrewingTemperature}";
+        }
+    }
+}
