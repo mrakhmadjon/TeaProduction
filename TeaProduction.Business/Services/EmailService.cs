26e10c83d9999656c242b82f6d5abbc13f9d5a2a
Akhmadjon <mrjohnaccaount@gmail.com>
2024-05-11 15:15:41 +0200

    Email service was implemented, ready to use

--- TeaProduction.Business/Services/EmailService.cs
+++ TeaProduction.Business/Services/EmailService.cs
@@ -1,4 +1,6 @@
-﻿using TeaProduction.Business.Interfaces;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using TeaProduction.Business.Interfaces;
 using TeaProduction.Business.Models;
 
 namespace TeaProduction.Business.Services
@@ -6,19 +8,105 @@ namespace TeaProduction.Business.Services
     public class EmailService : IEmailInterface
     {
         private readonly ILoggerInterface loggerInterface;
+        private readonly EmailConfigurationModel emailConfigurationModel;
 
-        public EmailService(ILoggerInterface loggerInterface)
+        public EmailService(ILoggerInterface loggerInterface,
+            EmailConfigurationModel emailConfigurationModel)
         {
             this.loggerInterface = loggerInterface;
+            this.emailConfigurationModel = emailConfigurationModel;
         }
         public void SendEmail(EmailMessageModel message)
         {
-            throw new NotImplementedException();
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
         }
 
-        public Task SendEmailAsync(EmailMessageModel message)
+        public async Task SendEmailAsync(EmailMessageModel message)
         {
-            throw new NotImplementedException();
+            var mailMessage = CreateEmailMessage(message);
+
+            await SendAsync(mailMessage);
+        }
+        private MimeMessage CreateEmailMessage(EmailMessageModel message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(emailConfigurationModel.From, emailConfigurationModel.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content) };
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                byte[] fileBytes;
+                foreach (var attachment in message.Attachments)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        attachment.CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+
+                    bodyBuilder.Attachments.Add(attachment.FileName, fileBytes, ContentType.Parse(attachment.ContentType));
+                }
+            }
+
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(emailConfigurationModel.SmtpServer, emailConfigurationModel.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(emailConfigurationModel.UserName, emailConfigurationModel.Password);
+
+                    client.Send(mailMessage);
+                }
+                catch (Exception ex)
+                {
+                    loggerInterface.Log(ex, "Error in sending email !!!");
+
+                }
+                finally
+                {
+                    loggerInterface.Log("Email Successfully send", Enums.LogLevel.Information);
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(emailConfigurationModel.SmtpServer, emailConfigurationModel.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(emailConfigurationModel.UserName, emailConfigurationModel.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    loggerInterface.Log("Error in sending email !!!", Enums.LogLevel.Error);
+                }
+                finally
+                {
+                    loggerInterface.Log("Email Successfully send", Enums.LogLevel.Information);
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+
+                }
+            }
         }
     }
 }
